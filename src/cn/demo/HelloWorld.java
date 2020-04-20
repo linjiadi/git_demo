fc62a1f65f4538c1a3baa516bb4969a60e7f975b
linjiadi <975456219@qq.com>
2020-04-20 22:13:29 +0800

    修改输出语句

--- src/cn/demo/HelloWorld.java
+++ src/cn/demo/HelloWorld.java
@@ -6,6 +6,6 @@ package cn.demo;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("heeloworld");
+        System.out.println("heeloworld123");
     }
 }
